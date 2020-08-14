401aa82280335338629eacd83b997b958e536d3d
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-14 14:38:50 +0100

    particles added for a better looking background image, css changed to ensure particles are below everything else

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js'
 import './App.css';
+
+const particlesEffect = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesEffect}
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
