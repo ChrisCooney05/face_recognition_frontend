4019234d920d111430b26ee74989c2c51aa7ac80
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-20 10:11:38 +0100

    able to pass through multiple bounding boxes toi face recognition

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,21 +1,34 @@
-import React from 'react'
-import './FaceRecognition.css'
+import React from "react";
+import "./FaceRecognition.css";
 
 function FaceRecognition({ imageUrl, box }) {
-  if (imageUrl !== '') {
+  console.log(box);
+  if (imageUrl !== "") {
     return (
-      <div className='center ma' alt='img'>
-        <div className='absolute mt2'>
-          <img src={imageUrl} alt='img' width='500px' height='auto' id='inputImage' />
-          <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+      <div className="center ma" alt="img">
+        <div className="absolute mt2">
+          <img
+            src={imageUrl}
+            alt="img"
+            width="500px"
+            height="auto"
+            id="inputImage"
+          />
+          <div
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          ></div>
         </div>
       </div>
     );
   } else {
-    return (
-      <div></div>
-    );
+    return <div></div>;
   }
 }
 
-export default FaceRecognition
+export default FaceRecognition;
